d520ae94c87ac4bdaa6f7d459406226d50827d6e
Jon Stuebe <jon@smartrent.com>
2019-09-06 06:57:00 -0700

    added ability to pass options

--- index.js
+++ index.js
@@ -1,49 +1,76 @@
-'use strict'
-var assert = require('assert')
+"use strict";
+var assert = require("assert");
 
 /**
  * html webpack preconnect plugin
  * @class
  */
 function HtmlWebpackPreconnectPlugin(options) {
-  assert.equal(options, undefined, 'The ResourceHintWebpackPlugin does not accept any options')
+  assert.equal(
+    options,
+    undefined,
+    "The ResourceHintWebpackPlugin does not accept any options"
+  );
 }
 
-const addPreconnectLinks = function (htmlPluginData, callback) {
+const addPreconnectLinks = function(htmlPluginData, callback) {
   var origins = htmlPluginData.plugin.options.preconnect;
-  assert.equal(origins instanceof Array, true, new TypeError('origins need array'));
-  origins.forEach(function (origin) {
+  assert.equal(
+    origins instanceof Array,
+    true,
+    new TypeError("origins need array")
+  );
+  origins.forEach(function(origin) {
     // webpack config may contain quotos, remove that
-    var href = origin.replace(/['"]+/g, '');
     var tag = {
-      tagName: 'link',
-      selfClosingTag: false,
-      attributes: {
-        rel: 'preconnect',
-        href: href,
-        crossorigin: ''
-      }
+      tagName: "link",
+      selfClosingTag: false
     };
+    var defaultAttributes = {
+      rel: "preconnect",
+      crossorigin: ""
+    };
+    if (typeof origin === "string") {
+      var href = origin.replace(/['"]+/g, "");
+      tag = Object.assign(tag, {
+        attributes: Object.assign(defaultAttributes, {
+          href: href
+        })
+      });
+    } else {
+      if (origin.attributes) {
+        origin.attributes = Object.assign(defaultAttributes, origin.attributes);
+      }
+      tag = Object.assign(tag, origin);
+    }
     htmlPluginData.head.push(tag);
   });
   callback(null, htmlPluginData);
-}
+};
 
-HtmlWebpackPreconnectPlugin.prototype.apply = function (compiler) {
+HtmlWebpackPreconnectPlugin.prototype.apply = function(compiler) {
   // Webpack 4
   if (compiler.hooks) {
-    compiler.hooks.compilation.tap('htmlWebpackPreconnectPlugin', function(compilation) {
+    compiler.hooks.compilation.tap("htmlWebpackPreconnectPlugin", function(
+      compilation
+    ) {
       // Hook into the html-webpack-plugin processing
-      compilation.hooks.htmlWebpackPluginAlterAssetTags.tapAsync('htmlWebpackPreconnectPlugin', addPreconnectLinks)
-    })
+      compilation.hooks.htmlWebpackPluginAlterAssetTags.tapAsync(
+        "htmlWebpackPreconnectPlugin",
+        addPreconnectLinks
+      );
+    });
 
-  // Webpack 3
+    // Webpack 3
   } else {
-    compiler.plugin('compilation', function (compilation) {
+    compiler.plugin("compilation", function(compilation) {
       // Hook into the html-webpack-plugin processing
-      compilation.plugin('html-webpack-plugin-alter-asset-tags', addPreconnectLinks)
-    })
+      compilation.plugin(
+        "html-webpack-plugin-alter-asset-tags",
+        addPreconnectLinks
+      );
+    });
   }
-}
+};
 
-module.exports = HtmlWebpackPreconnectPlugin
+module.exports = HtmlWebpackPreconnectPlugin;
